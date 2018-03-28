5506e01f934980cc5d0d3d4bde10cb75ef43294e
StanislasTran <transtanislas@gmail.com>
2018-03-28 16:23:04 +0200

    test library
    test de la library

--- src/io/github/oliviercailloux/y2018/jconfs/ConferenceReader.java
+++ src/io/github/oliviercailloux/y2018/jconfs/ConferenceReader.java
@@ -1,6 +1,35 @@
 package io.github.oliviercailloux.y2018.jconfs;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
+import ezvcard.VCard;
+import ezvcard.ValidationWarning;
+import ezvcard.io.text.*;
+
+/**
+ * A ConferenceReader is an Object that enables to read a vcf file and create an
+ * object Conference
+ * 
+ * @author stanislas
+ *
+ */
 public class ConferenceReader {
 
+	private VCardReader reader;
+
+	public ConferenceReader(File VCFfile) throws FileNotFoundException  {
+		this.reader = new VCardReader(new File("Jean_Dupont.vcf"));
+		
+		
+	}
+
+	public Conference createConference() throws IOException {
+		System.out.println(reader.readNext());
+		VCard Vcard =reader.readNext();
+		System.out.println("test");
+		return null;
+	}
 }
--- src/io/github/oliviercailloux/y2018/jconfs/Test.java
+++ src/io/github/oliviercailloux/y2018/jconfs/Test.java
@@ -1,8 +1,28 @@
 package io.github.oliviercailloux.y2018.jconfs;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import ezvcard.Ezvcard;
+import ezvcard.VCard;
+import ezvcard.io.text.VCardReader;
 public class Test  {
-	public static void main(String [] args) {
+	public static void main(String [] args) throws IOException {
 		PathStep test =new PathStep("Paris","La Courneuve",TransportType.TRAIN);
 		System.out.println("hello");
+		
+		ConferenceReader test2=new ConferenceReader(new File ("Jean_Dupont.vcf"));
+		test2.createConference();
+		
+		VCard test4=new VCard();
+		VCardReader reader=new VCardReader("Jean_Dupont.vcf");
+		System.out.println("ici2");
+		System.out.println(reader);
+		while((test4=reader.readNext())!=null){
+			System.out.println("ici");
+			System.out.println(reader);
+		}
+	
+		
 	}
 }
