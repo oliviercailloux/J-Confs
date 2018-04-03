d4ba4014ccb70fed61f3b1383e82a366009a952a
berkanimus <berkanimuss@gmail.com>
2018-04-03 18:48:37 +0200

    Mustapha & Stanislas File 2

--- src/io/github/oliviercailloux/y2018/jconfs/Main.java
+++ src/io/github/oliviercailloux/y2018/jconfs/Main.java
@@ -1,16 +1,16 @@
 package io.github.oliviercailloux.y2018.jconfs;
 
-public class main  {
-	public static void main(String [] args) throws Exception {
+public class Main  {
+	public static void main (String [] args) throws Exception {
 		Conference conf;
-		conf = ReadCalendarFiles.createConference("basic.ics");
+		conf=ReadCalendarFiles.createConference("basic.ics");
 		System.out.println(" On va Lire l'objet conference créé à partir du fichier ics ");
 		System.out.println(conf);
 		System.out.println(" \n \n  On va Lire le fichier ics");
 		ReadCalendarFiles.ReadCalendarFile("basic.ics");
-	
-		
-	
-		
+
+
+
+
 	}
 }
--- src/io/github/oliviercailloux/y2018/jconfs/ReadCalendarFiles.java
+++ src/io/github/oliviercailloux/y2018/jconfs/ReadCalendarFiles.java
@@ -1,9 +1,13 @@
 package io.github.oliviercailloux.y2018.jconfs;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.time.LocalDate;
 import java.util.Iterator;
 import javax.xml.bind.ValidationException;
+
+import org.apache.commons.io.IOUtils;
 import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.ParserException;
 import net.fortuna.ical4j.model.Component;
@@ -11,10 +15,14 @@ import net.fortuna.ical4j.model.Property;
 
 
 /**
+ * This class allows to read and iCalelndar file and creates a conference object from a parsed iCalendar file
  * @author berkani-mustapha
  *
  */
 public class ReadCalendarFiles {
+
+
+	private static InputStream fin;
 
 
 	/** Parse an iCalendar object with ical4j API
@@ -24,27 +32,30 @@ public class ReadCalendarFiles {
 	 * @throws ParserException
 	 */
 	public static  void ReadCalendarFile(String filePath) throws IOException, ParserException, ValidationException {
-		FileInputStream fin = new FileInputStream(filePath);
+		FileInputStream fin1 = new FileInputStream(filePath);
 
 		CalendarBuilder builder = new CalendarBuilder();
 
-		net.fortuna.ical4j.model.Calendar calendar = builder.build(fin);
+		net.fortuna.ical4j.model.Calendar calendar = builder.build(fin1);
 
-		// Iterating over the calendar 
+		// Iterating over the calendar component
 		for (Iterator<Component> i = calendar.getComponents().iterator(); i.hasNext();) {
 			Component component = i.next();
 			System.out.println("Component [" + component.getName() + "]");
-
+			// Iterating over the component property
 			for (Iterator<Property> j = component.getProperties().iterator(); j.hasNext();) {
-				Property property = (Property) j.next();
+				Property property = j.next();
 				System.out.println("Property [" + property.getName() + ", " + property.getValue() + "]");
 			}
 		}
 	}
 
-	
+
 
 	/**
+	 * generate a localDate from string parametre
+	 * This function is needed since the ical4j property DTSTART is a string 
+	 * so we have to convert it into a localDate format  
 	 * @param date
 	 * @return
 	 */
@@ -61,35 +72,40 @@ public class ReadCalendarFiles {
 	 * @throws ParserException
 	 * @throws ValidationException
 	 */
-	public static  Conference createConference(String filepath) throws IOException, ParserException, ValidationException {
+
+	public static  Conference createConference(String filepath) throws IOException, ParserException {
+
 		Conference conf = null;
-		FileInputStream fin = new FileInputStream(filepath);
-		CalendarBuilder builder = new CalendarBuilder();
-		net.fortuna.ical4j.model.Calendar calendar = builder.build(fin);
+		fin = new java.io.FileInputStream(filepath);
+		try {
+			fin = new java.io.BufferedInputStream(fin);
+			CalendarBuilder builder = new CalendarBuilder();
+			net.fortuna.ical4j.model.Calendar calendar = builder.build(fin);
 
-		for (Iterator j = calendar.getProperties().iterator(); j.hasNext();) {
-			Property property = (Property) j.next();
+			for (Iterator j = calendar.getProperties().iterator(); j.hasNext();) {
+				Property property = (Property) j.next();
 
-			conf = new Conference(Property.URL );
+				conf = new Conference(Property.URL );
 
-			if(property.getName()==Property.DTSTART)
-				conf.setStartDate(property.getValue());
+				if(property.getName()==Property.DTSTART)
+					conf.setStartDate(property.getValue());
 
-			if(property.getName()==Property.DTEND)
-				conf.setEndDate(property.getValue());
+				if(property.getName()==Property.DTEND)
+					conf.setEndDate(property.getValue());
 
-			if(property.getName()==Property.REGION)
-				conf.setRegion(property.getValue());
+				if(property.getName()==Property.REGION)
+					conf.setRegion(property.getValue());
 
-			if(property.getName()==Property.COUNTRY)
-				conf.setCountry(property.getValue());
+				if(property.getName()==Property.COUNTRY)
+					conf.setCountry(property.getValue());
 
-			//add field for Title
-			//add field for free-registration
+				//add field for Title
+				//add field for free-registration
+			}
+
+		} finally {
+			IOUtils.closeQuietly(fin);
 		}
-
-		fin.close();
 		return conf; 
 	}
-
 }
