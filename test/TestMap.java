cb89e4930b39db9ad9df267fbf2f8de4f3687430
StanislasTran <transtanislas@gmail.com>
2018-04-07 20:19:27 +0200

    add comment
    add comment

--- test/TestMap.java
+++ test/TestMap.java
@@ -6,12 +6,20 @@ import java.io.File;
 
 import org.mapsforge.map.reader.MapFile;
 
+/**
+ * 
+ * Create a MapFile frome ressources1 directory, print "done" and close it
+ *
+ */
 public class TestMap {
 	public static void main(String [] args) {
 		
 		
 		File fichier=new File ("resources1/world.map");
-		
+		/*
+		 * opens the given map file,
+		 *  reads its header data and validates them.
+		 */
 		MapFile map =new MapFile(fichier);
 		
 		System.out.println("done");
