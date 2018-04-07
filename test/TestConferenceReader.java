9896357b49d15783ab859a4bf5cc4685cfb9aa72
StanislasTran <transtanislas@gmail.com>
2018-04-07 20:00:17 +0200

    Using equals
    using the method equals from Conference

--- test/TestConferenceReader.java
+++ test/TestConferenceReader.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.net.URL;
 import java.text.ParseException;
 
 import io.github.oliviercailloux.y2018.jconfs.Conference;
@@ -7,14 +8,36 @@ import net.fortuna.ical4j.data.ParserException;
 
 public class TestConferenceReader {
 
+	/*
+	 * a test for ConferenceReader Methods which print success or fail
+	 */
 	public static void main(String[] args) throws IOException, ParserException, ParseException {
 
+		
+		/*
+		 * create a conference object by reading the file test2.ics in resources1 folder
+		 * 
+		 */
 		Conference conf = ConferenceReader.createConference("resources1/test2.ics");
-		System.out.println(" On va Lire l'objet conference créé à partir du fichier ics ");
-		System.out.println(conf);
-		System.out.println(conf.toString());
-		System.out.println(" \n \n  On va Lire le fichier ics");
-		ConferenceReader.ReadCalendarFile("resources1/test2.ics");
+		
+		/*
+		 * create a conference object to compare with conf 
+		 * 
+		 */
+		
+		Conference conf2 =new Conference (new URL ("http://www.test.com/"));
+		conf2.setCity("Paris");
+		conf2.setCountry("France");
+		conf2.setEndDate("19970714T170000Z");
+		conf2.setFeeRegistration(22.60);
+		conf2.setStartDate("19970714T170000Z");
+		conf2.setTitle("java");
+		
+		if(conf.equals(conf2)) {
+			System.out.println("success");
+		}else {
+			System.out.println("fail");
+		}
 		
 		System.out.println("done");
 	}
