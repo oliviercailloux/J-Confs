fd27b05112781595890273f230f984ffb065cf82
StanislasTran <transtanislas@gmail.com>
2018-04-17 21:09:50 +0200

    change test to use classpath
    "srcs/test/Resources/test2.ics"--> "test2.ics"

--- src/test/java/TestConferenceReader.java
+++ src/test/java/TestConferenceReader.java
@@ -8,14 +8,16 @@ import net.fortuna.ical4j.data.ParserException;
 public class TestConferenceReader {
 
 	public static void main(String[] args) throws IOException, ParserException, ParseException {
-				
-		Conference conf = ConferenceReader.createConference("src/test/resources/test2.ics");
+		
+		Conference conf = ConferenceReader.createConference("test2.ics");
 		System.out.println(" On va Lire l'objet conference créé à partir du fichier ics ");
 		System.out.println(conf);
 		System.out.println(conf.toString());
 		System.out.println(" \n \n  On va Lire le fichier ics");
 
-		ConferenceReader.ReadCalendarFile("src/test/resources/test2.ics");
+		ConferenceReader.ReadCalendarFile("test2.ics");
+		
+		ConferenceReader.ReadCalendarFile("test2.ics");
 		System.out.println("donee");
 	}
 }
