898e90730ce89c3ca587406dcf2d80fd55ad0be3
Hương Quỳnh <quynhhuongnguyen25@gmail.com>
2018-04-18 20:54:12 +0200

    Changed the classe MissionOrder.java

--- src/main/java/io/github/oliviercailloux/y2018/jconfs/MissionOrder.java
+++ src/main/java/io/github/oliviercailloux/y2018/jconfs/MissionOrder.java
@@ -1,10 +1,9 @@
 package io.github.oliviercailloux.y2018.jconfs;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 import org.odftoolkit.simple.SpreadsheetDocument;
-import org.odftoolkit.simple.table.Cell;
+
 /**
  * 
  * @author huong,camille
@@ -21,11 +20,7 @@ public class MissionOrder {
 	private void generateSpreadsheetDocument() throws Exception {
 		try (InputStream inputStream = MissionOrder.class.getResourceAsStream("missionOrder.ods");
 				SpreadsheetDocument spreadsheetDoc = SpreadsheetDocument.loadDocument(inputStream)) {
-			System.out.println("File opened and ready to close!!!");
-			//Cell positionCell = spreadsheetDoc.getTableByName("B").getCellByPosition("A1");
-			//positionCell.setStringValue("ploum");
-			//spreadsheetDoc.save("demo9s.ods");
-			
+			System.out.println("File opened and ready to close!!!");	
 	        inputStream.close();
 		}
 		catch (Exception e){
