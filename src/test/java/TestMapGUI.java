aecd385e7b47926b9fa08f9e76abf21c83e7e1d9
StanislasTran <transtanislas@gmail.com>
2018-05-23 21:14:39 +0200

    add test in comment

--- src/test/java/TestMapGUI.java
+++ src/test/java/TestMapGUI.java
@@ -16,7 +16,7 @@ import io.github.oliviercailloux.y2018.jconfs.MapGUI;
 public class TestMapGUI {
 	public static void main(String[]args) throws NullPointerException, IllegalArgumentException, IOException  {
 		MapGUI GUI=new MapGUI("world.map",new LatLong( 39.913818, 116.363625)); //endPoint (pékin latLong)
-		
+	//	MapGUI GUI=new MapGUI();//test online map
 		GUI.display();
 		
 		
