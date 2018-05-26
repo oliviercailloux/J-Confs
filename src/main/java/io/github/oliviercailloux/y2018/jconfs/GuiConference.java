5555bf0317b9c87fada033a0e5c970ae7fdff721
Hương Quỳnh <quynhhuongnguyen25@gmail.com>
2018-05-27 01:12:03 +0200

    MAJ GUI Conference

--- src/main/java/io/github/oliviercailloux/y2018/jconfs/GuiConference.java
+++ src/main/java/io/github/oliviercailloux/y2018/jconfs/GuiConference.java
@@ -1,13 +1,18 @@
 package io.github.oliviercailloux.y2018.jconfs;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.text.ParseException;
+import java.util.Arrays;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.VerifyEvent;
+import org.eclipse.swt.events.VerifyListener;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.DateTime;
@@ -16,6 +21,8 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
+import io.github.oliviercailloux.y2018.jconfs.Conference;
+import io.github.oliviercailloux.y2018.jconfs.ConferenceWriter;
 import net.fortuna.ical4j.data.ParserException;
 import net.fortuna.ical4j.validate.ValidationException;
 
@@ -48,15 +55,56 @@ public class GuiConference {
 		labelFee.setText("Registration Fee : ");
 		Text textFee = new Text(shell, SWT.BORDER);
 		
+		//allow only positive integers as input and not allow special characters like letter 
+		textFee.addVerifyListener(new VerifyListener() {
+			 public void verifyText(VerifyEvent e) {
+				 String string = e.text;
+			      char[] chars = new char[string.length()];
+			      string.getChars(0, chars.length, chars, 0);
+			      for (int i = 0; i < chars.length; i++) {
+			         if (!('0' <= chars[i] && chars[i] <= '9')) {
+			            e.doit = false;
+			            return;
+			         }
+			      }
+			   }
+		 });
+		
+		//not allow the integers
 		Label labelCity = new Label(shell, SWT.NONE);
 		labelCity.setText("City : ");
 		Text textCity = new Text(shell, SWT.BORDER);
+		textCity.addVerifyListener(new VerifyListener() {
+			 public void verifyText(VerifyEvent e) {
+				 String string = e.text;
+			      char[] chars = new char[string.length()];
+			      string.getChars(0, chars.length, chars, 0);
+			      for (int i = 0; i < chars.length; i++) {
+			         if ('0' <= chars[i] && chars[i] <= '9') {
+			            e.doit = false;
+			            return;
+			         }
+			      }
+			   }
+		 });
 		
-
+		////not allow the integers
 		Label labelCountry = new Label(shell, SWT.NONE);
 		labelCountry.setText("Country : ");
 		Text textCountry = new Text(shell, SWT.BORDER);
-		
+		textCountry.addVerifyListener(new VerifyListener() {
+			 public void verifyText(VerifyEvent e) {
+				 String string = e.text;
+			      char[] chars = new char[string.length()];
+			      string.getChars(0, chars.length, chars, 0);
+			      for (int i = 0; i < chars.length; i++) {
+			         if ('0' <= chars[i] && chars[i] <= '9') {
+			            e.doit = false;
+			            return;
+			         }
+			      }
+			   }
+		 });
 		//create Date Selection as a drop-down
 		Label labelDateStart = new Label(shell, SWT.NONE);
 		labelDateStart.setText("Date Start : ");
@@ -69,51 +117,115 @@ public class GuiConference {
 		Button buttonSubmit = new Button(shell, SWT.PUSH);
 		buttonSubmit.setText("Submit");
 		buttonSubmit.addSelectionListener(new SelectionAdapter() {
-			// the function proceed() executed once we press the button Submit
-			public void widgetSelected(SelectionEvent event) {
-				try {
-					proceed();
-				} catch (ValidationException e) {
-					e.printStackTrace();
-				} catch (ParseException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (ParserException e) {
-					e.printStackTrace();
-				} catch (URISyntaxException e) {
-					e.printStackTrace();
-				}
-				
-			}
 			//this function save the value in the fields of GUI in a conference and write-read a ICalendar
-			private void proceed() throws ParseException, ValidationException, IOException, ParserException, URISyntaxException {
-				URL url = new URL("http://www.conference.com");
+			public void widgetSelected(SelectionEvent event)  {
+				URL url = null;
+				try {
+					url = new URL("http://www.conference.com");
+				} catch (MalformedURLException e1) {
+					e1.printStackTrace();
+				}
 				Conference conf = new Conference(url);
 				String title = textTitle.getText();
 				Double fee = Double.parseDouble(textFee.getText());
 				String city = textCity.getText();
 				String country = textCountry.getText();
+				// add "O" before the day and month if they are 1, 2, 3, 4, 5, 6, 7, 8, 9
+				String[] array = {"1","2", "3", "4", "5", "6", "7", "8", "9"};
+				
 				String dStart = Integer.toString(dateStart.getDay());
-				String mStart = Integer.toString(dateStart.getMonth());
+				String mStart = Integer.toString(dateStart.getMonth()+1);
 				String yStart = Integer.toString(dateStart.getYear());
-				String start = yStart + "-" + mStart + "-" + dStart;
+				String start = "";
+				String dnew = "";
+				String mnew = "";
+				boolean dayStart = Arrays.asList(array).contains(dStart);
+				boolean monthStart = Arrays.asList(array).contains(mStart);
+				if(dayStart && !monthStart ){
+					dnew = "0"+dStart;
+			        start = dnew + "/" + mStart + "/" + yStart;
+			            	           
+				}
+			    else if (!dayStart && monthStart){
+			    	mnew = "0"+mStart;
+			    	start = dStart + "/" + mnew + "/" + yStart;
+				}
+			    else if (dayStart && monthStart){
+			    	dnew = "0"+dStart;
+			    	mnew = "0"+mStart;
+					start = dnew + "/" + mnew + "/" + yStart;
+			    }
+			        else {
+			        	start = dStart + "/" + mStart + "/" + yStart;
+			   }
+				
+			    
 				String dEnd = Integer.toString(dateEnd.getDay());
-				String mEnd = Integer.toString(dateEnd.getMonth());
+				String mEnd = Integer.toString(dateEnd.getMonth()+1);
 				String yEnd = Integer.toString(dateEnd.getYear());
-				String end = yEnd + "-" + mEnd + "-" + dEnd;
+				
+				String end = "";
+				String dnew1 = "";
+				String mnew1 = "";
+				boolean dayEnd = Arrays.asList(array).contains(dEnd);
+				boolean monthEnd = Arrays.asList(array).contains(mEnd);
+				if(dayEnd && !monthEnd ){
+					dnew1 = "0"+dEnd;
+			         end = dnew1 + "/" + mEnd + "/" + yEnd;
+			            	           
+					}
+			        else if (!dayEnd && monthEnd){
+			            	mnew1 = "0"+mEnd;
+							end = dEnd + "/" + mnew1 + "/" + yEnd;
+							
+			         }
+			        else if (dayEnd && monthEnd){
+			        	dnew1 = "0"+dEnd;
+		            	mnew1 = "0"+mEnd;
+						end = dnew1 + "/" + mnew1 + "/" + yEnd;
+						
+			        }
+			        else {
+			            	end = dEnd + "/" + mEnd + "/" + yEnd;
+			            }
+				    
 				conf.setCity(city);
 				conf.setCountry(country);
-				conf.setStartDate(start);
-				conf.setEndDate(end);
+				try {
+					conf.setStartDate(start);
+				} catch (ParseException e1) {
+					e1.printStackTrace();
+				}
+				try {
+					conf.setEndDate(end);
+				} catch (ParseException e1) {
+					e1.printStackTrace();
+				}
+				
+				
 				conf.setFeeRegistration(fee);
 				conf.setTitle(title);
-				ConferenceWriter.writeCalendarFiles(conf);
-				System.out.println("The iCalendar has created in the file mycalendar.ics ");
+				 
+
+							try {
+								ConferenceWriter.writeCalendarFiles(conf);
+								
+							} catch (ValidationException | ParseException | IOException | ParserException
+									| URISyntaxException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						
 				
+				System.out.println("The iCalendar has created in the file mycalendar.ics ");
+			
 			}
+			
 		});
 		
+		Color col = new Color(display, 145, 201, 169);
+		shell.setBackground(col);
+		col.dispose();
 		// tear down the SWT window
 		shell.pack();
 		shell.open();
