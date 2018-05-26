4155e3139b540ea7bd88bd44c620162fec36d71b
Hương Quỳnh <quynhhuongnguyen25@gmail.com>
2018-05-27 01:01:43 +0200

    MAJ Conference

--- src/main/java/io/github/oliviercailloux/y2018/jconfs/Conference.java
+++ src/main/java/io/github/oliviercailloux/y2018/jconfs/Conference.java
@@ -1,9 +1,10 @@
 package io.github.oliviercailloux.y2018.jconfs;
 import java.net.URL;
 import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
-import net.fortuna.ical4j.model.DateTime;
 
 
 /**
@@ -12,8 +13,8 @@ import net.fortuna.ical4j.model.DateTime;
 public class Conference {
 	private URL url;
 	private String title;
-	private DateTime startDate;
-	private DateTime endDate;
+	private LocalDate startDate;
+	private LocalDate endDate;
 	private Double registrationFee;
 	private String country;
 	private String city;
@@ -47,7 +48,7 @@ public class Conference {
 	 * This is a getter which return the date start  	
 	 * @return startDate
 	 */
-	public DateTime getStartDate() {
+	public LocalDate getStartDate() {
 		return startDate;
 	}
 
@@ -57,14 +58,15 @@ public class Conference {
 	 * @throws ParseException 
 	 */
 	public void setStartDate(String startDate) throws ParseException {
-		this.startDate = new DateTime(startDate);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		this.startDate = LocalDate.parse(startDate, formatter);
 	}
 
 	/**
 	 * This is a getter which return the date end 	
 	 * @return endDate
 	 */
-	public  DateTime getEndDate() {
+	public  LocalDate getEndDate() {
 		return endDate;
 	}
 
@@ -74,7 +76,8 @@ public class Conference {
 	 * @throws ParseException 
 	 */
 	public void setEndDate( String endDate) throws ParseException {
-		this.endDate = new DateTime(endDate);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		this.endDate = LocalDate.parse(endDate, formatter);
 	}
 
 	/**
@@ -132,12 +135,36 @@ public class Conference {
 	public Conference(URL url) {
 		this.url =Objects.requireNonNull(url);
 		this.title =new String("");
-		this.startDate = new DateTime();
-		this.endDate =  new DateTime();
+		this.startDate = LocalDate.now();
+		this.endDate = LocalDate.now();
 		this.registrationFee = 0.0;
 		this.country = "";
 		this.city="";
 	}
+	
+	@Override
+	/**
+	 * Compare the conference to object by comparing all attributes 
+	 */
+	public boolean equals(Object obj) {
+		if (obj instanceof Conference) {
+			Conference conference2 = (Conference) obj;
+			System.out.println(toString());
+			System.out.println(conference2.toString());
+			if (title.equals(conference2.title) && url.equals(conference2.url)
+					&& startDate.equals(conference2.startDate) && endDate.equals(conference2.endDate)
+					&& registrationFee == conference2.registrationFee && city.equals(conference2.city)
+					&& country.equals(conference2.country)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	@Override
+	public int hashCode(){
+		return Objects.hash(url,title,registrationFee,startDate,endDate,country,city);
+	}
+
 
 	@Override
 	public String toString() {
