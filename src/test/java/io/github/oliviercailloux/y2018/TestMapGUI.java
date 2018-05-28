efd321929d4e4893b8150728641700efe5ddb78a
StanislasTran <transtanislas@gmail.com>
2018-05-28 13:09:35 +0200

    UNIK

---
+++ src/test/java/io/github/oliviercailloux/y2018/TestMapGUI.java
@@ -0,0 +1,25 @@
+package io.github.oliviercailloux.y2018;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.UUID;
+
+import org.mapsforge.core.model.LatLong;
+import org.mapsforge.map.awt.graphics.AwtGraphicFactory;
+import org.mapsforge.map.awt.util.AwtUtil;
+import org.mapsforge.map.awt.view.MapView;
+import org.mapsforge.map.layer.cache.TileCache;
+import org.mapsforge.map.layer.download.TileDownloadLayer;
+import org.mapsforge.map.layer.download.tilesource.OpenStreetMapMapnik;
+
+import io.github.oliviercailloux.y2018.jconfs.MapGUI;
+
+public class TestMapGUI {
+	public static void main(String[]args) throws NullPointerException, IllegalArgumentException, IOException  {
+		MapGUI GUI=new MapGUI("world.map",new LatLong( 39.913818, 116.363625)); //endPoint (pékin latLong)
+	//	MapGUI GUI=new MapGUI();//test online map
+		GUI.display();
+		
+		
+	}
+}
