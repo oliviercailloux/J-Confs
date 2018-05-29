36a30fe8f2244815d795275a77f28cee860ab610
StanislasTran <transtanislas@gmail.com>
2018-05-29 11:36:59 +0200

    adapt to localDate

--- src/test/java/io/github/oliviercailloux/y2018/TestConferenceReader.java
+++ src/test/java/io/github/oliviercailloux/y2018/TestConferenceReader.java
@@ -1,4 +1,5 @@
 package io.github.oliviercailloux.y2018;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,18 +14,26 @@ import net.fortuna.ical4j.data.ParserException;
 public class TestConferenceReader {
 
 	public static void main(String[] args) throws IOException, ParserException, ParseException {
-		File test=new File (TestConferenceReader.class.getClassLoader().getResource("test2.ics").getFile());
+
+		File test = new File(TestConferenceReader.class.getClassLoader().getResource("icaldata/test2.ics").getFile());
+
 		try (InputStreamReader reader = new InputStreamReader(new FileInputStream(test))) {
 			Conference conf = ConferenceReader.createConference(reader);
+
 			System.out.println(" On va Lire l'objet conference créé à partir du fichier ics ");
 			System.out.println(conf);
 			System.out.println(conf.toString());
+		}
+
+		try (InputStreamReader reader = new InputStreamReader(new FileInputStream(test))) {
 			System.out.println(" \n \n  On va Lire le fichier ics");
 
 			ConferenceReader.ReadCalendarFile(reader);
 
-			ConferenceReader.ReadCalendarFile(reader);
 			System.out.println("donee");
+
+			System.out.println("ii");
 		}
 	}
+
 }
