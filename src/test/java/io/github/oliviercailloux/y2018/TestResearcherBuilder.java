07f5f839cdfd39e589bb4d8207eb07a97794f804
StanislasTran <transtanislas@gmail.com>
2018-06-02 22:23:23 +0200

    Junit test for ResearcherBuilder

---
+++ src/test/java/io/github/oliviercailloux/y2018/TestResearcherBuilder.java
@@ -0,0 +1,57 @@
+package io.github.oliviercailloux.y2018;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Logger;
+import io.github.oliviercailloux.y2018.jconfs.Researcher;
+import io.github.oliviercailloux.y2018.jconfs.ResearcherBuilder;
+
+/**
+ * a Junit test for ResearcherBuilder class
+ * @author stanislas
+ *
+ */
+public class TestResearcherBuilder {
+	private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(TestResearcherBuilder.class);
+
+	/**
+	 * A test for the methode create
+	 * @throws ClassNotFoundException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws ClassCastException
+	 */
+	@Test
+	public final void createTest() throws ClassNotFoundException, InstantiationException, IllegalAccessException, ClassCastException {
+		Researcher oCailloux = ResearcherBuilder.create("ocailloux");
+		LOGGER.debug("create Researcher object");
+
+		assertEquals("CAILLOUX",oCailloux.getNom());
+		assertEquals("Olivier",oCailloux.getPrenom());
+		assertEquals("+33 1 44 05 46 53",oCailloux.getTelephone());
+		assertEquals("MIDO-LAMSADE",oCailloux.getGroupe());
+		assertEquals("olivier.cailloux@lamsade.dauphine.fr",oCailloux.getMail());
+		assertEquals("P405 ter",oCailloux.getBureau());
+		
+		
+
+	}
+
+	/**
+	 * a main to try the method create 
+	 * @param args
+	 * @throws ClassNotFoundException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws ClassCastException
+	 */
+	public static void main(String[] args)
+			throws ClassNotFoundException, InstantiationException, IllegalAccessException, ClassCastException {
+		Researcher test = ResearcherBuilder.create("ocailloux");
+		System.out.println(test.toString());
+
+	}
+}
