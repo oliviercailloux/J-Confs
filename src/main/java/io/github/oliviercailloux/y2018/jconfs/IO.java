8cb573b25e759bd9deaf1bdcc08a46ed7afb154f
Hương Quỳnh <quynhhuongnguyen25@gmail.com>
2018-06-22 23:58:37 +0200

    Generate YS

---
+++ src/main/java/io/github/oliviercailloux/y2018/jconfs/IO.java
@@ -0,0 +1,11 @@
+package io.github.oliviercailloux.y2018.jconfs;
+
+import java.util.Scanner;
+
+public class IO {
+	public Scanner scanner;
+
+	public IO() {
+		scanner = new Scanner(System.in);
+	}
+}
