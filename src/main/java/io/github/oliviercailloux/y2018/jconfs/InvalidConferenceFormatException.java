10573887984b381f32490c0cb99c4d33148bd33a
nikolaspaci <nikolaspaci>
2019-05-30 14:49:34 +0200

    add javadoc to custom exception and modify ressources of test/res

--- src/main/java/io/github/oliviercailloux/y2018/jconfs/InvalidConferenceFormatException.java
+++ src/main/java/io/github/oliviercailloux/y2018/jconfs/InvalidConferenceFormatException.java
@@ -1,12 +1,17 @@
 package io.github.oliviercailloux.y2018.jconfs;
 
 
+/**
+ * @author nikola
+ * Custom exception use for a conference creation
+ */
 public class InvalidConferenceFormatException extends Exception {
 	/**
-	 * 
+	 * Default Serial ID
 	 */
 	private static final long serialVersionUID = 1L;
 
+	
 	public InvalidConferenceFormatException(String errorMessage) {
 		super(errorMessage);
 	}
