7ee02b2ca2da01c131417422d80ac793f18686f4
aliliKM <alison.messan@gmail.com>
2019-06-13 22:41:13 +0200

    Researcher constructor

--- src/main/java/io/github/oliviercailloux/y2018/jconfs/ResearcherBuilder.java
+++ src/main/java/io/github/oliviercailloux/y2018/jconfs/ResearcherBuilder.java
@@ -31,7 +31,7 @@ public class ResearcherBuilder {
 	public ResearcherBuilder() {
 		LOGGER.debug("create an empty Reasarcher");
 	}
-	
+
 	/**
 	 *  create is a static factory method to create Researcher from dauphine website
 	 * @param login
@@ -42,44 +42,44 @@ public class ResearcherBuilder {
 	 * @throws ClassCastException
 	 */
 	public static Researcher  create(String login) throws ClassNotFoundException, InstantiationException, IllegalAccessException, ClassCastException {
-	Client client =ClientBuilder.newClient();
-	WebTarget webTarget=client.target("https://www.ent.dauphine.fr/annuaire/index.php?param0=fiche&param1="+login);
-	LOGGER.debug("webTarget initialisation succed");
-	String result=webTarget.request(MediaType.TEXT_PLAIN).get(String.class);
-	LOGGER.debug("request succed",result);
+		Client client =ClientBuilder.newClient();
+		WebTarget webTarget=client.target("https://www.ent.dauphine.fr/annuaire/index.php?param0=fiche&param1="+login);
+		LOGGER.debug("webTarget initialisation succed");
+		String result=webTarget.request(MediaType.TEXT_PLAIN).get(String.class);
+		LOGGER.debug("request succed",result);
 
-	DOMImplementationRegistry registry = DOMImplementationRegistry.newInstance();
-	DOMImplementationLS impl = (DOMImplementationLS) registry.getDOMImplementation("LS");
-	
-	LSParser builder = impl.createLSParser(DOMImplementationLS.MODE_SYNCHRONOUS, null);
-	LSInput input= impl.createLSInput();
+		DOMImplementationRegistry registry = DOMImplementationRegistry.newInstance();
+		DOMImplementationLS impl = (DOMImplementationLS) registry.getDOMImplementation("LS");
 
-	input.setStringData(result);
+		LSParser builder = impl.createLSParser(DOMImplementationLS.MODE_SYNCHRONOUS, null);
+		LSInput input= impl.createLSInput();
 
-	Document doc=builder.parse(input);
-	LOGGER.debug("item created",doc);
+		input.setStringData(result);
 
-	String searcher=Objects.requireNonNull(doc.getElementsByTagName("h4").item(0).getFirstChild().getTextContent());
-	String  nom=searcher.split(" ")[1];
-	String prenom=searcher.split(" ")[0];
-	Researcher researcher=new Researcher(nom,prenom);
-	NodeList listElements = doc.getElementsByTagName("li");
-	researcher.setFunction(Objects.requireNonNull(listElements.item(2).getFirstChild().getTextContent()));
-	researcher.setPhone(Objects.requireNonNull(listElements.item(4).getFirstChild().getTextContent().trim()));
-	researcher.setOffice(Objects.requireNonNull(listElements.item(8).getFirstChild().getTextContent().trim()));
-	researcher.setMail(Objects.requireNonNull(listElements.item(10).getFirstChild().getTextContent().trim()));
-	listElements=doc.getElementsByTagName("a");
-	
-	String group="";
-	//add every groups 
-	for (int i=3;i<listElements.getLength();i++) {
-		if(i!=3)
-			group+="-";
-		group+=listElements.item(i).getFirstChild().getTextContent();
-		LOGGER.debug("group "+i+" added",group);
+		Document doc=builder.parse(input);
+		LOGGER.debug("item created",doc);
 
-	}
-	researcher.setGroup(group);
-	return researcher;
+		String searcher=Objects.requireNonNull(doc.getElementsByTagName("h4").item(0).getFirstChild().getTextContent());
+		String lastName=searcher.split(" ")[1];
+		String firstName=searcher.split(" ")[0];
+		Researcher researcher=null;
+		NodeList listElements = doc.getElementsByTagName("li");
+		String function=Objects.requireNonNull(listElements.item(2).getFirstChild().getTextContent());
+		String phone=Objects.requireNonNull(listElements.item(4).getFirstChild().getTextContent().trim());
+		String office=Objects.requireNonNull(listElements.item(8).getFirstChild().getTextContent().trim());
+		String mail=Objects.requireNonNull(listElements.item(10).getFirstChild().getTextContent().trim());
+		listElements=doc.getElementsByTagName("a");
+
+		String group="";
+		//add every groups 
+		for (int i=3;i<listElements.getLength();i++) {
+			if(i!=3)
+				group+="-";
+			group+=listElements.item(i).getFirstChild().getTextContent();
+			LOGGER.debug("group "+i+" added",group);
+
+		}
+		researcher = new Researcher(lastName, firstName, function, phone, office, mail, group);
+		return researcher;
 	}
 }
