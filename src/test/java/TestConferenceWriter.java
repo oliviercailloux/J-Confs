65bb2714be0b2815f5b194f7338a4bfdd266c38a
aliliKM <alison.messan@gmail.com>
2019-06-13 22:09:49 +0200

    test

--- src/test/java/TestConferenceWriter.java
+++ src/test/java/TestConferenceWriter.java
@@ -5,12 +5,6 @@ import io.github.oliviercailloux.y2018.jconfs.ConferenceWriter;
 public class TestConferenceWriter {
 	public static void main (String [] args) throws Exception {
 		Conference conferenceToWrite = new Conference(new URL("http://www.test.com"));
-		conferenceToWrite.setTitle("coucou");
-		conferenceToWrite.setCountry("America");
-		conferenceToWrite.setCity("New_YorkS");
-		conferenceToWrite.setEndDate("04/01/2018");
-		conferenceToWrite.setStartDate("06/06/2017");
-		conferenceToWrite.setFeeRegistration(12.5);
 		ConferenceWriter.writeCalendarFiles(conferenceToWrite.getTitle(),conferenceToWrite);
 		
 		System.out.println("done");
