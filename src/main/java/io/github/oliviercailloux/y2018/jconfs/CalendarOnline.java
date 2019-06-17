9333680f79708bc77908f7732e94a70fe288c5e7
nikolaspaci <nikolaspaci>
2019-06-17 22:50:11 +0200

    add variable

--- src/main/java/io/github/oliviercailloux/y2018/jconfs/CalendarOnline.java
+++ src/main/java/io/github/oliviercailloux/y2018/jconfs/CalendarOnline.java
@@ -1,4 +1,5 @@
 package io.github.oliviercailloux.y2018.jconfs;
+
 import java.util.List;
 import java.util.Set;
 import java.util.Iterator;
@@ -20,42 +21,37 @@ import org.osaf.caldav4j.methods.HttpClient;
 import org.osaf.caldav4j.model.request.CalendarQuery;
 import org.osaf.caldav4j.util.GenerateQuery;
 
-
 public class CalendarOnline {
+	final private static String userNameFruux = "b3297431374";
+	final private static String passwordFruux = "4j9z8c7no08f";
+	final private static String calendarIDFruux="d1010138-3ae5-4ba5-b0fd-dcee1e5fcdf2";
 
-  public static Set<Conference> getonlineConferences() throws CalDAV4JException, InvalidConferenceFormatException
-  {
-    HttpClient httpClient = new HttpClient();
-    httpClient.getHostConfiguration().setHost("dav.fruux.com", 443, "https");
-    String username = "b3297431374";
-    String calendarID="d1010138-3ae5-4ba5-b0fd-dcee1e5fcdf2";
-    UsernamePasswordCredentials httpCredentials = new UsernamePasswordCredentials(username, "4j9z8c7no08f");
-    httpClient.getState().setCredentials(AuthScope.ANY, httpCredentials);
-    httpClient.getParams().setAuthenticationPreemptive(true);
+	public static Set<Conference> getonlineConferences() throws CalDAV4JException, InvalidConferenceFormatException {
+		HttpClient httpClient = new HttpClient();
+		httpClient.getHostConfiguration().setHost("dav.fruux.com", 443, "https");
+		UsernamePasswordCredentials httpCredentials = new UsernamePasswordCredentials(userNameFruux, passwordFruux);
+		httpClient.getState().setCredentials(AuthScope.ANY, httpCredentials);
+		httpClient.getParams().setAuthenticationPreemptive(true);
 
+		CalDAVCollection collection = new CalDAVCollection("/calendars/" + userNameFruux + "/" + calendarIDFruux,
+				(HostConfiguration) httpClient.getHostConfiguration().clone(), new CalDAV4JMethodFactory(),
+				CalDAVConstants.PROC_ID_DEFAULT);
 
-    CalDAVCollection collection = new CalDAVCollection(
-        "/calendars/"+ username +"/"+calendarID,
-        (HostConfiguration) httpClient.getHostConfiguration().clone(),
-        new CalDAV4JMethodFactory(),
-        CalDAVConstants.PROC_ID_DEFAULT
-        );
+		GenerateQuery gq = new GenerateQuery();
+		CalendarQuery calendarQuery = gq.generate();
+		List<Calendar> calendars = collection.queryCalendars(httpClient, calendarQuery);
+		Set<Conference> listeconfuser = new LinkedHashSet<>();
 
-    GenerateQuery gq=new GenerateQuery();
-    CalendarQuery calendarQuery = gq.generate();
-    List<Calendar>calendars = collection.queryCalendars(httpClient, calendarQuery);
-	Set<Conference> listeconfuser=new LinkedHashSet<>();
+		for (Calendar calendar : calendars) {
+			ComponentList<VEvent> componentList = calendar.getComponents(Component.VEVENT);
+			Iterator<VEvent> eventIterator = componentList.iterator();
+			while (eventIterator.hasNext()) {
+				VEvent ve = eventIterator.next();
+				listeconfuser.add(ConferenceReader.createConference(ve));
+				System.out.println("Event: " + ve.toString());
+			}
+		}
+		return listeconfuser;
 
-    for (Calendar calendar : calendars) {
-      ComponentList<VEvent> componentList = calendar.getComponents(Component.VEVENT);
-      Iterator<VEvent> eventIterator = componentList.iterator();
-      while (eventIterator.hasNext()) {
-        VEvent ve = eventIterator.next();
-        listeconfuser.add(ConferenceReader.createConference(ve));
-        System.out.println("Event: "+ ve.toString());
-       }
-    }
-	return listeconfuser;
-
-  }
+	}
 }
