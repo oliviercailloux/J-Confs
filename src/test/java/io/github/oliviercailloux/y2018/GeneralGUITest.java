1acc800519e21383c01afc1ec1b143c7116360fc
aliliKM <alison.messan@gmail.com>
2019-06-20 10:16:30 +0200

    comments

--- src/test/java/io/github/oliviercailloux/y2018/GeneralGUITest.java
+++ src/test/java/io/github/oliviercailloux/y2018/GeneralGUITest.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import io.github.oliviercailloux.y2018.jconfs.GeneralGUI;
 
 public class GeneralGUITest {
-	public static void main (String[]args)  {
-		//
-	}
-}
+	//empty block - suppress this class
+ }
 
