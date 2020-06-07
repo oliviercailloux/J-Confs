548d45bafccdc6ed47c3c8cd8f8a085c1b1958f5
sebastienbourg <sebastien.bourg@dauphine.eu>
2020-06-07 12:32:03 +0200

    add javadoc

--- src/main/java/io/github/oliviercailloux/jconfs/location/Address.java
+++ src/main/java/io/github/oliviercailloux/jconfs/location/Address.java
@@ -1,5 +1,13 @@
 package io.github.oliviercailloux.jconfs.location;
 
+/**
+ * This class allows you to store an address define as a string address and the
+ * latitude and longitude associate wth.
+ * 
+ * @author Floryan Kieffer
+ * 
+ * 
+ */
 public class Address {
 
 	private String address;
@@ -16,6 +24,13 @@ public class Address {
 		return new Address();
 	}
 
+	/**
+	 * Factory method which creates a Address instance
+	 *
+	 * @param address
+	 * @param latitude
+	 * @param longitude
+	 */
 	public static Address newInstanceAddressComplete(String address, String latitude, String longitude) {
 		return new Address(address, latitude, longitude);
 	}
@@ -30,32 +45,69 @@ public class Address {
 		this.latitude = "";
 	}
 
+	/**
+	 * Private constructor
+	 *
+	 * @param address
+	 * @param latitude
+	 * @param longitude
+	 */
 	private Address(String address, String latitude, String longitude) {
 		this.address = address;
 		this.latitude = latitude;
 		this.longitude = longitude;
 	}
 
+	/**
+	 * This method return the address name
+	 * 
+	 * @return address
+	 */
 	public String getAddress() {
 		return address;
 	}
 
+	/**
+	 * This method set the address name
+	 * 
+	 * @param address
+	 */
 	public void setAddress(String address) {
 		this.address = address;
 	}
 
+	/**
+	 * This method return the latitude of the address object
+	 * 
+	 * @return latitude
+	 */
 	public String getLatitude() {
 		return latitude;
 	}
 
+	/**
+	 * This method set the address latitude
+	 * 
+	 * @param latitude
+	 */
 	public void setLatitude(String latitude) {
 		this.latitude = latitude;
 	}
 
+	/**
+	 * This method return the longitude of the address object
+	 * 
+	 * @return longitude
+	 */
 	public String getLongitude() {
 		return longitude;
 	}
 
+	/**
+	 * This method set the address longitude
+	 * 
+	 * @param longitude
+	 */
 	public void setLongitude(String longitude) {
 		this.longitude = longitude;
 	}
