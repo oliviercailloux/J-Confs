b5ba85aa12a62f6c4dc40e09d8602c6b43a67fd4
saidaniyaa <saidaniyaa.muthukumarasamy@dauphine.eu>
2022-04-13 20:04:07 +0200

    error fixed with a new way to open a file

--- src/main/java/io/github/oliviercailloux/jconfs/conference/ConferencesFromICal.java
+++ src/main/java/io/github/oliviercailloux/jconfs/conference/ConferencesFromICal.java
@@ -6,7 +6,9 @@ import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Path;
 import java.time.Instant;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -14,44 +16,50 @@ import net.fortuna.ical4j.data.ParserException;
 
 public class ConferencesFromICal implements ConferencesRetriever {
 
-  @Override
-  public Set<Conference> retrieve()
-      throws IOException, ParserException, InvalidConferenceFormatException {
-    String filePath = ConferencesFromICal.class.getClassLoader().getResource("icaldata").getFile();
-    File ressourcesDirectory = new File(filePath);
-    File[] fileList = ressourcesDirectory.listFiles();
-    Set<Conference> setOfConf = new LinkedHashSet<>();
-    for (File file : fileList) {
-      if (file.getName().endsWith(".ics")) {
-        try (InputStreamReader reader = new InputStreamReader(new FileInputStream(file))) {
-          setOfConf.addAll(ConferenceReader.readConferences(reader));
-        }
-      }
-    }
-    return setOfConf;
-  }
+	@Override
+	public Set<Conference> retrieve() throws IOException, ParserException, InvalidConferenceFormatException {
+		String filePath = ConferencesFromICal.class.getClassLoader().getResource("icaldata").getFile();
+		File ressourcesDirectory = new File(filePath);
+		File[] fileList = ressourcesDirectory.listFiles();
+		Set<Conference> setOfConf = new LinkedHashSet<>();
+		for (File file : fileList) {
+			if (file.getName().endsWith(".ics")) {
+				try (InputStreamReader reader = new InputStreamReader(new FileInputStream(file))) {
+					setOfConf.addAll(ConferenceReader.readConferences(reader));
+				}
+			}
+		}
+		return setOfConf;
+	}
 
-  @Override
-  public Set<Conference> retrieve(Instant minDate, Instant maxDate)
-      throws IOException, ParserException, InvalidConferenceFormatException {
-    Set<Conference> setOfAllConf = retrieve();
-    Set<Conference> setOfConfFiltred = new LinkedHashSet<>();
-    for (Conference conf : setOfAllConf) {
-      if (conf.getStartDate().isAfter(minDate) && conf.getEndDate().isBefore(maxDate)) {
-        setOfConfFiltred.add(conf);
-      }
-    }
-    return setOfConfFiltred;
-  }
+	@Override
+	public Set<Conference> retrieve(Instant minDate, Instant maxDate)
+			throws IOException, ParserException, InvalidConferenceFormatException {
+		Set<Conference> setOfAllConf = retrieve();
+		Set<Conference> setOfConfFiltred = new LinkedHashSet<>();
+		for (Conference conf : setOfAllConf) {
+			if (conf.getStartDate().isAfter(minDate) && conf.getEndDate().isBefore(maxDate)) {
+				setOfConfFiltred.add(conf);
+			}
+		}
+		return setOfConfFiltred;
+	}
 
-  @Override
-  public Set<Conference> retrieve(String fileName)
-      throws InvalidConferenceFormatException, IOException, ParserException {
-    Preconditions.checkNotNull(fileName);
-    URL urlcalendar = ConferenceReader.class.getResource(fileName + ".ics");
-    try (FileReader reader = new FileReader(new File(urlcalendar.getFile()))) {
-      return ConferenceReader.readConferences(reader);
-    }
+	@Override
+	public Set<Conference> retrieve(String fileName)
+			throws InvalidConferenceFormatException, IOException, ParserException {
+		Preconditions.checkNotNull(fileName);
+		URL urlcalendar = ConferenceReader.class.getResource(fileName + ".ics");
+		Path p;
+		try {
+			p = Path.of(urlcalendar.toURI());
+		} catch (URISyntaxException e) {
+			throw new IllegalStateException(e);
+		}
+		String s = p.toString();
+		try (FileReader reader = new FileReader(new File(s))) {
+			return ConferenceReader.readConferences(reader);
+		}
 
-  }
+	}
 }
