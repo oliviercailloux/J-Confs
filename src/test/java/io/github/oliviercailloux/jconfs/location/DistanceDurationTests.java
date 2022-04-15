8a798e842aeddc879e9aedcbbd2a514598a751f4
saidaniyaa <saidaniyaa.muthukumarasamy@dauphine.eu>
2022-04-15 14:43:20 +0200

    javadoc updated for testGetDistanceDuration()

--- src/test/java/io/github/oliviercailloux/jconfs/location/DistanceDurationTests.java
+++ src/test/java/io/github/oliviercailloux/jconfs/location/DistanceDurationTests.java
@@ -10,10 +10,14 @@ class DistanceDurationTests {
 
 	/**
 	 * 
-	 * Unit test in order to verify the good get of distance and duration. Carreful
-	 * this test depend on API calculation so it might fail in the future. Important
-	 * : A sleep of 1 second as been added due to the request limit per second of
-	 * the API free use.
+	 * Unit test in order to verify the good get of distance and duration. We are
+	 * checking that the distance between the departure and the arrival is between
+	 * 5000 m and 15000 m because it can fluctuate depending on the API used. It's
+	 * the same idea for the duration (between 1000 seconds and 2000 seconds) and
+	 * the number of steps (between 100 and 200 steps)
+	 * 
+	 * Important: A sleep of 1 second as been added due to the request limit per
+	 * second of the API free use.
 	 * 
 	 * @throws ApiException
 	 * @throws InterruptedException
