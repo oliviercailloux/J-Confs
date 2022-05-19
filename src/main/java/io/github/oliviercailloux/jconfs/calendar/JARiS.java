c5aa0e160b3bb78635a4917fa009f395296871ad
JulienHaderer <julien.haderer@dauphine.eu>
2022-05-19 22:35:41 +0200

    Format

--- src/main/java/io/github/oliviercailloux/jconfs/calendar/JARiS.java
+++ src/main/java/io/github/oliviercailloux/jconfs/calendar/JARiS.java
@@ -8,71 +8,59 @@ import java.nio.file.Path;
 import com.google.common.collect.ImmutableSet;
 import java.util.*;
 
-
 public class JARiS {
-  
-  public static enum FruuxKeysCredential {
-    API_USERNAME,
-    API_PASSWORD,
-    API_URL,
-    API_CalendarID,
-  }
-  
-  public static ImmutableCompleteMap<FruuxKeysCredential, String> myAuth;
-  public static CredentialsReader<FruuxKeysCredential> reader;
-  
-  
-  public ImmutableCompleteMap<FruuxKeysCredential, String> editFruuxKeys(String project_username , String project_password , String project_url , String project_calendarID){
-    
-    System.setProperty("API_USERNAME", project_username);
-    System.setProperty("API_PASSWORD", project_password);
-    System.setProperty("API_URL", project_url);
-    System.setProperty("API_CalendarID", project_calendarID);
-    reader = CredentialsReader.using(FruuxKeysCredential.class, Path.of("my file.txt"));
-    myAuth = reader.getCredentials();
-    return myAuth ;
 
-  }
-  
-  
-public void printFruuxKeys(){
-    
-    System.out.println("API_USERNAME : " + myAuth.get(FruuxKeysCredential.API_USERNAME));
-    System.out.println("API_PASSWORD : " + myAuth.get(FruuxKeysCredential.API_PASSWORD));
-    System.out.println("API_URL : " + myAuth.get(FruuxKeysCredential.API_URL));
-    System.out.println("API_CalendarID : " + myAuth.get(FruuxKeysCredential.API_CalendarID));
-    
-  }
+	public static enum FruuxKeysCredential {
+		API_USERNAME, API_PASSWORD, API_URL, API_CalendarID,
+	}
 
-public String getFruuxUsername() {
-  
-  return myAuth.get(FruuxKeysCredential.API_USERNAME);
-  
-}
+	public static ImmutableCompleteMap<FruuxKeysCredential, String> myAuth;
+	public static CredentialsReader<FruuxKeysCredential> reader;
 
-public String getFruuxPassword() {
-  
-  return myAuth.get(FruuxKeysCredential.API_PASSWORD);
-  
-}
+	public ImmutableCompleteMap<FruuxKeysCredential, String> editFruuxKeys(String project_username,
+			String project_password, String project_url, String project_calendarID) {
 
-public String getFruuxURL() {
-  
-  return myAuth.get(FruuxKeysCredential.API_URL);
-  
-}
+		System.setProperty("API_USERNAME", project_username);
+		System.setProperty("API_PASSWORD", project_password);
+		System.setProperty("API_URL", project_url);
+		System.setProperty("API_CalendarID", project_calendarID);
+		reader = CredentialsReader.using(FruuxKeysCredential.class, Path.of("my file.txt"));
+		myAuth = reader.getCredentials();
+		return myAuth;
 
-public String getFruuxCalendarID() {
-  
-  return myAuth.get(FruuxKeysCredential.API_CalendarID);
-  
-}
-  
-  
-  
-  
- 
+	}
 
+	public void printFruuxKeys() {
 
-  
+		System.out.println("API_USERNAME : " + myAuth.get(FruuxKeysCredential.API_USERNAME));
+		System.out.println("API_PASSWORD : " + myAuth.get(FruuxKeysCredential.API_PASSWORD));
+		System.out.println("API_URL : " + myAuth.get(FruuxKeysCredential.API_URL));
+		System.out.println("API_CalendarID : " + myAuth.get(FruuxKeysCredential.API_CalendarID));
+
+	}
+
+	public String getFruuxUsername() {
+
+		return myAuth.get(FruuxKeysCredential.API_USERNAME);
+
+	}
+
+	public String getFruuxPassword() {
+
+		return myAuth.get(FruuxKeysCredential.API_PASSWORD);
+
+	}
+
+	public String getFruuxURL() {
+
+		return myAuth.get(FruuxKeysCredential.API_URL);
+
+	}
+
+	public String getFruuxCalendarID() {
+
+		return myAuth.get(FruuxKeysCredential.API_CalendarID);
+
+	}
+
 }
--- src/test/java/io/github/oliviercailloux/jconfs/calendar/TestJARiS.java
+++ src/test/java/io/github/oliviercailloux/jconfs/calendar/TestJARiS.java
@@ -14,40 +14,37 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 public class TestJARiS {
 
-  @Test
-  public void testGetClassicalCredential() {
-    
-    String API_USERNAME = "API_USERNAME";
-    String API_PASSWORD = "API_PASSWORD";
-    System.setProperty(API_USERNAME, "username");
-    System.setProperty(API_PASSWORD, "password");
-   
-    CredentialsReader<ClassicalCredentials> reader = CredentialsReader.classicalReader();
-    Map<ClassicalCredentials, String> myAuth = reader.getCredentials();
-    
-    assertEquals(API_USERNAME,myAuth.get(ClassicalCredentials.API_USERNAME));
-    assertEquals(API_PASSWORD,myAuth.get(ClassicalCredentials.API_PASSWORD));
-    
-  }
-  
-  
-  @Test
-  public void testGetFruuxCredential() {
-    JARiS instance = new JARiS();
-    
-    final String test_username = "b3297393754";
-    final String test_password = "4pq8nzbhzugs";
-    final String test_url = "dav.fruux.com";
-    final String test_calendarID = "8b3ff300-b8ce-4d85-a255-76ea3dff1338";
-    
-    JARiS.myAuth = instance.editFruuxKeys(test_username,test_password,test_url,test_calendarID);
-    assertEquals(test_username,instance.getFruuxUsername());
-    assertEquals(test_password,instance.getFruuxPassword());
-    assertEquals(test_url,instance.getFruuxURL());
-    assertEquals(test_calendarID,instance.getFruuxCalendarID());
-    
-    
-  }
-  
-  
+	@Test
+	public void testGetClassicalCredential() {
+
+		String API_USERNAME = "API_USERNAME";
+		String API_PASSWORD = "API_PASSWORD";
+		System.setProperty(API_USERNAME, "username");
+		System.setProperty(API_PASSWORD, "password");
+
+		CredentialsReader<ClassicalCredentials> reader = CredentialsReader.classicalReader();
+		Map<ClassicalCredentials, String> myAuth = reader.getCredentials();
+
+		assertEquals(API_USERNAME, myAuth.get(ClassicalCredentials.API_USERNAME));
+		assertEquals(API_PASSWORD, myAuth.get(ClassicalCredentials.API_PASSWORD));
+
+	}
+
+	@Test
+	public void testGetFruuxCredential() {
+		JARiS instance = new JARiS();
+
+		final String test_username = "b3297393754";
+		final String test_password = "4pq8nzbhzugs";
+		final String test_url = "dav.fruux.com";
+		final String test_calendarID = "8b3ff300-b8ce-4d85-a255-76ea3dff1338";
+
+		JARiS.myAuth = instance.editFruuxKeys(test_username, test_password, test_url, test_calendarID);
+		assertEquals(test_username, instance.getFruuxUsername());
+		assertEquals(test_password, instance.getFruuxPassword());
+		assertEquals(test_url, instance.getFruuxURL());
+		assertEquals(test_calendarID, instance.getFruuxCalendarID());
+
+	}
+
 }
