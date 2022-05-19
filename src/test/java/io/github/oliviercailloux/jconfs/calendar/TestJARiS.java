b39a41d10ca9b6700d4ef8ba41e42650168dc738
JulienHaderer <julien.haderer@dauphine.eu>
2022-05-19 22:31:32 +0200

    Modification du test

--- src/test/java/io/github/oliviercailloux/jconfs/calendar/TestJARiS.java
+++ src/test/java/io/github/oliviercailloux/jconfs/calendar/TestJARiS.java
@@ -1,5 +1,6 @@
 package io.github.oliviercailloux.jconfs.calendar;
 
+import io.github.oliviercailloux.jaris.collections.ImmutableCompleteMap;
 import io.github.oliviercailloux.jaris.credentials.CredentialsReader;
 import io.github.oliviercailloux.jaris.credentials.CredentialsReader.ClassicalCredentials;
 import io.github.oliviercailloux.jconfs.calendar.JARiS.FruuxKeysCredential;
@@ -32,19 +33,18 @@ public class TestJARiS {
   
   @Test
   public void testGetFruuxCredential() {
-    
-    FruuxKeysCredential testCrendential = new CredentialsReader.FruuxKeysCredential();
+    JARiS instance = new JARiS();
     
     final String test_username = "b3297393754";
     final String test_password = "4pq8nzbhzugs";
     final String test_url = "dav.fruux.com";
     final String test_calendarID = "8b3ff300-b8ce-4d85-a255-76ea3dff1338";
     
-    CredentialsReader<FruuxKeysCredential> testAuth = editFruuxKeys(test_username,test_password,test_url,test_calendarID);
-    assertEquals(test_username,getFruuxUsername());
-    assertEquals(test_password,getFruuxUsername());
-    assertEquals(test_url,getFruuxUsername());
-    assertEquals(test_calendarID,getFruuxUsername());
+    JARiS.myAuth = instance.editFruuxKeys(test_username,test_password,test_url,test_calendarID);
+    assertEquals(test_username,instance.getFruuxUsername());
+    assertEquals(test_password,instance.getFruuxPassword());
+    assertEquals(test_url,instance.getFruuxURL());
+    assertEquals(test_calendarID,instance.getFruuxCalendarID());
     
     
   }
