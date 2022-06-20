ead460985b041750ea311b1173619d4433494410
Mikhal21 <mikhal.ifergan@dauphine.eu>
2022-06-20 15:30:33 +0200

    Adding more than one participants to a conference

--- src/main/java/io/github/oliviercailloux/jconfs/conference/ConferenceReader.java
+++ src/main/java/io/github/oliviercailloux/jconfs/conference/ConferenceReader.java
@@ -16,137 +16,142 @@ import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.Component;
 import net.fortuna.ical4j.model.ComponentList;
 import net.fortuna.ical4j.model.Property;
+import net.fortuna.ical4j.model.PropertyList;
 import net.fortuna.ical4j.model.component.CalendarComponent;
 
 /**
- * This class allows to read and iCalelndar file and creates a conference object from a parsed
- * iCalendar file
+ * This class allows to read and iCalelndar file and creates a conference object
+ * from a parsed iCalendar file
  * 
  */
 public class ConferenceReader {
 
-  /**
-   * Parse an iCalendar object with ical4j API function took from source :
-   * https://www.programcreek.com/java-api-examples/?api=net.fortuna.ical4j.model.property.Method
-   * 
-   * @param filePath
-   * @throws IOException
-   * @throws ParserException
-   */
+	/**
+	 * Parse an iCalendar object with ical4j API function took from source :
+	 * https://www.programcreek.com/java-api-examples/?api=net.fortuna.ical4j.model.property.Method
+	 * 
+	 * @param filePath
+	 * @throws IOException
+	 * @throws ParserException
+	 */
 
-  public static void readCalendarFile(Reader read)
-      throws IOException, ParserException, NumberFormatException {
-    CalendarBuilder builder = new CalendarBuilder();
-    Calendar calendar = builder.build(read);
-    for (Component component : calendar.getComponents()) {
-      System.out.println("Component [" + component.getName() + "]");
-      for (Property property : component.getProperties()) {
-        System.out.println("Property [" + property.getName() + ", " + property.getValue() + "]");
-      }
-    }
-  }
+	public static void readCalendarFile(Reader read) throws IOException, ParserException, NumberFormatException {
+		CalendarBuilder builder = new CalendarBuilder();
+		Calendar calendar = builder.build(read);
+		for (Component component : calendar.getComponents()) {
+			System.out.println("Component [" + component.getName() + "]");
+			for (Property property : component.getProperties()) {
+				System.out.println("Property [" + property.getName() + ", " + property.getValue() + "]");
+			}
+		}
+	}
 
-  /**
-   * We parse a calendar component to create a conference
-   * 
-   * @param confCompo it's a calendar component that contains the data of one conference
-   * @return a conference
-   * @throws MalformedURLException
-   * @throws IOException
-   * @throws ParserException
-   * @throws NumberFormatException
-   */
-  public static Conference createConference(Component confCompo) throws MalformedURLException {
-    Conference conf = null;
-    ConferenceBuilder theBuild = new ConferenceBuilder();
+	/**
+	 * We parse a calendar component to create a conference
+	 * 
+	 * @param confCompo it's a calendar component that contains the data of one
+	 *                  conference
+	 * @return a conference
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 * @throws ParserException
+	 * @throws NumberFormatException
+	 */
+	public static Conference createConference(Component confCompo) throws MalformedURLException {
+		Conference conf = null;
+		ConferenceBuilder theBuild = new ConferenceBuilder();
 
-    URL confURL;
-    String[] location;
-    String[] description;
-    if (!confCompo.getProperties("URL").isEmpty()) {
-      confURL = new URL(confCompo.getProperty("URL").getValue());
-      theBuild.setUrl(confURL);
-    }
-    if (!confCompo.getProperties("LOCATION").isEmpty()) {
-      location = confCompo.getProperty("LOCATION").getValue().split(",");
-      String city = location[0];
-      String country = location[1];
-      theBuild.setCity(city);
-      theBuild.setCountry(country);
-    }
-    if (!confCompo.getProperties("DESCRIPTION").isEmpty()) {
-      description = confCompo.getProperty("DESCRIPTION").getValue().split("/");
-      for (String ele : description) {
-        if (ele.contains("Fee")) {
-          Double feeRegistration = Double.parseDouble(ele.substring(ele.indexOf(":") + 1));
-          theBuild.setRegistrationFee(feeRegistration.intValue());
-        } else {
-          if (ele.contains("Participants")) {
-            String oneParticipant = ele.substring(ele.indexOf(":") + 1);
-            theBuild.setParticipant(oneParticipant);
-          }
-        }
-      }
-    }
+		URL confURL;
+		String[] location;
+		String[] description;
+		String participant;
+		if (!confCompo.getProperties("URL").isEmpty()) {
+			confURL = new URL(confCompo.getProperty("URL").getValue());
+			theBuild.setUrl(confURL);
+		}
+		if (!confCompo.getProperties("LOCATION").isEmpty()) {
+			location = confCompo.getProperty("LOCATION").getValue().split(",");
+			String city = location[0];
+			String country = location[1];
+			theBuild.setCity(city);
+			theBuild.setCountry(country);
+		}
+		if (!confCompo.getProperties("DESCRIPTION").isEmpty()) {
+			description = confCompo.getProperty("DESCRIPTION").getValue().split("/");
+			for (String ele : description) {
+				if (ele.contains("Fee")) {
+					Double feeRegistration = Double.parseDouble(ele.substring(ele.indexOf(":") + 1));
+					theBuild.setRegistrationFee(feeRegistration.intValue());
+				}
+			}
+		}
+		if (!confCompo.getProperties("ATTENDEE").isEmpty()) {
+			PropertyList<Property> participants = confCompo.getProperties("ATTENDEE");
+			for (Property oneParticipant : participants) {
+				participant = oneParticipant.getValue();
+				theBuild.setParticipant(participant);
+			}
+		}
 
-    if (!confCompo.getProperties("UID").isEmpty()) {
-      String uid = confCompo.getProperty("UID").getValue();
-      theBuild.setUid(uid);
-    }
+		if (!confCompo.getProperties("UID").isEmpty()) {
+			String uid = confCompo.getProperty("UID").getValue();
+			theBuild.setUid(uid);
+		}
 
-    String title = confCompo.getProperty("SUMMARY").getValue();
+		String title = confCompo.getProperty("SUMMARY").getValue();
 
-    String stringDTSTART = convertDate(confCompo.getProperty("DTSTART").getValue());
-    String stringDTEND = convertDate(confCompo.getProperty("DTEND").getValue());
-    LocalDate start = null;
-    LocalDate end = null;
+		String stringDTSTART = convertDate(confCompo.getProperty("DTSTART").getValue());
+		String stringDTEND = convertDate(confCompo.getProperty("DTEND").getValue());
+		LocalDate start = null;
+		LocalDate end = null;
 
-    try {
-      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-      start = LocalDate.parse(stringDTSTART, formatter);
-      end = LocalDate.parse(stringDTEND, formatter);
-    } catch (Exception e) {
-      throw new IllegalArgumentException("Date impossible to put in the conference", e);
-    }
-    theBuild.setTitle(title).setStartDate(start.atStartOfDay(ZoneOffset.UTC).toInstant())
-        .setEndDate(end.atStartOfDay(ZoneOffset.UTC).toInstant());
-    conf = theBuild.build();
+		try {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+			start = LocalDate.parse(stringDTSTART, formatter);
+			end = LocalDate.parse(stringDTEND, formatter);
+		} catch (Exception e) {
+			throw new IllegalArgumentException("Date impossible to put in the conference", e);
+		}
+		theBuild.setTitle(title).setStartDate(start.atStartOfDay(ZoneOffset.UTC).toInstant())
+				.setEndDate(end.atStartOfDay(ZoneOffset.UTC).toInstant());
+		conf = theBuild.build();
 
-    return conf;
-  }
+		return conf;
+	}
 
-  /**
-   * We will import a set of conferences contain in a ical
-   * 
-   * @param read contain data of the user's calendar
-   * @return a list of the conferences of the user
-   * @throws IOException
-   * @throws ParserException
-   * @throws NumberFormatException
-   */
-  public static Set<Conference> readConferences(Reader reader) throws IOException, ParserException {
-    CalendarBuilder builder = new CalendarBuilder();
-    Calendar calendar = builder.build(reader);
-    Set<Conference> listeconfuser = new LinkedHashSet<>();
-    ComponentList<CalendarComponent> conflist = calendar.getComponents(Component.VEVENT);
-    for (int i = 0; i < conflist.size(); i++) {
-      listeconfuser.add(createConference(conflist.get(i)));
-    }
-    return listeconfuser;
+	/**
+	 * We will import a set of conferences contain in a ical
+	 * 
+	 * @param read contain data of the user's calendar
+	 * @return a list of the conferences of the user
+	 * @throws IOException
+	 * @throws ParserException
+	 * @throws NumberFormatException
+	 */
+	public static Set<Conference> readConferences(Reader reader) throws IOException, ParserException {
+		CalendarBuilder builder = new CalendarBuilder();
+		Calendar calendar = builder.build(reader);
+		Set<Conference> listeconfuser = new LinkedHashSet<>();
+		ComponentList<CalendarComponent> conflist = calendar.getComponents(Component.VEVENT);
+		for (int i = 0; i < conflist.size(); i++) {
+			listeconfuser.add(createConference(conflist.get(i)));
+		}
+		return listeconfuser;
 
-  }
+	}
 
-  /**
-   * this function transform a date to an another pattern of date
-   * 
-   * @param date Not {@code null} it's the date that we want to change its pattern.
-   * 
-   * @return dateformated it's the date with the good pattern
-   */
-  public static String convertDate(String date) {
-    DateTimeFormatter formatBefore = DateTimeFormatter.ofPattern("yyyyMMdd");
-    DateTimeFormatter formatAfter = DateTimeFormatter.ofPattern("dd/MM/yyy");
-    String dateformated = LocalDate.parse(date, formatBefore).format(formatAfter);
-    return dateformated;
-  }
+	/**
+	 * this function transform a date to an another pattern of date
+	 * 
+	 * @param date Not {@code null} it's the date that we want to change its
+	 *             pattern.
+	 * 
+	 * @return dateformated it's the date with the good pattern
+	 */
+	public static String convertDate(String date) {
+		DateTimeFormatter formatBefore = DateTimeFormatter.ofPattern("yyyyMMdd");
+		DateTimeFormatter formatAfter = DateTimeFormatter.ofPattern("dd/MM/yyy");
+		String dateformated = LocalDate.parse(date, formatBefore).format(formatAfter);
+		return dateformated;
+	}
 }
--- src/main/java/io/github/oliviercailloux/jconfs/conference/ConferenceWriter.java
+++ src/main/java/io/github/oliviercailloux/jconfs/conference/ConferenceWriter.java
@@ -9,6 +9,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.text.ParseException;
 import java.util.Objects;
+import java.util.Set;
 import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.CalendarOutputter;
 import net.fortuna.ical4j.data.ParserException;
@@ -35,199 +36,205 @@ import net.fortuna.ical4j.validate.ValidationException;
 
 public class ConferenceWriter {
 
-  /**
-   * Open the specified ics file as an Calendar object
-   * 
-   * @param calFile not {@code null}.
-   * @return not {@code null}.
-   * @throws IOException
-   * @throws ParserException
-   */
-  private static Calendar openCalendar(String calFile) throws IOException, ParserException {
-    Objects.requireNonNull(calFile);
-    URL urlcalendar = ConferenceWriter.class.getResource(calFile + ".ics");
-    try (FileReader reader = new FileReader(new File(urlcalendar.getFile()))) {
-      CalendarBuilder builder = new CalendarBuilder();
-      Calendar calendar = builder.build(reader);
-      return calendar;
-    }
-  }
+	/**
+	 * Open the specified ics file as an Calendar object
+	 * 
+	 * @param calFile not {@code null}.
+	 * @return not {@code null}.
+	 * @throws IOException
+	 * @throws ParserException
+	 */
+	private static Calendar openCalendar(String calFile) throws IOException, ParserException {
+		Objects.requireNonNull(calFile);
+		URL urlcalendar = ConferenceWriter.class.getResource(calFile + ".ics");
+		try (FileReader reader = new FileReader(new File(urlcalendar.getFile()))) {
+			CalendarBuilder builder = new CalendarBuilder();
+			Calendar calendar = builder.build(reader);
+			return calendar;
+		}
+	}
 
-  /**
-   * Delete the conference of the ics file (if it exists)
-   * 
-   * @param calFile not {@code null}.
-   * @param conference not {@code null}.
-   * @throws IOException
-   * @throws ParserException
-   * @throws URISyntaxException
-   */
-  public static void deleteConference(String calFile, Conference conference)
-      throws IOException, ParserException, URISyntaxException {
-    Objects.requireNonNull(calFile);
-    Objects.requireNonNull(conference);
+	/**
+	 * Delete the conference of the ics file (if it exists)
+	 * 
+	 * @param calFile    not {@code null}.
+	 * @param conference not {@code null}.
+	 * @throws IOException
+	 * @throws ParserException
+	 * @throws URISyntaxException
+	 */
+	public static void deleteConference(String calFile, Conference conference)
+			throws IOException, ParserException, URISyntaxException {
+		Objects.requireNonNull(calFile);
+		Objects.requireNonNull(conference);
 
-    Calendar calendar = new Calendar();
-    Calendar newCalendar;
+		Calendar calendar = new Calendar();
+		Calendar newCalendar;
 
-    Conference tempConf;
-    calendar = openCalendar(calFile);
+		Conference tempConf;
+		calendar = openCalendar(calFile);
 
-    ComponentList<CalendarComponent> confList = calendar.getComponents("X-CONFERENCE");
-    for (int i = 0; i < confList.size(); i++) {
-      tempConf = ConferenceReader.createConference(confList.get(i));
-      if (tempConf.equals(conference)) {
-        confList.remove(i);
-      }
-    }
+		ComponentList<CalendarComponent> confList = calendar.getComponents("X-CONFERENCE");
+		for (int i = 0; i < confList.size(); i++) {
+			tempConf = ConferenceReader.createConference(confList.get(i));
+			if (tempConf.equals(conference)) {
+				confList.remove(i);
+			}
+		}
 
-    newCalendar = new Calendar(confList);
-    saveIcsFile(newCalendar, calFile);
-  }
+		newCalendar = new Calendar(confList);
+		saveIcsFile(newCalendar, calFile);
+	}
 
-  /**
-   * Transform a Conference into a Property List to be used after in VEvent(in the case of
-   * communication with server ) or Event (In the case of IcsFile) this is the specific one for
-   * fruux. To communicate with fruux we need to specificate order
-   * 
-   * @param conference not {@code null}.
-   * 
-   * @throws URISyntaxException
-   */
+	/**
+	 * Transform a Conference into a Property List to be used after in VEvent(in the
+	 * case of communication with server ) or Event (In the case of IcsFile) this is
+	 * the specific one for fruux. To communicate with fruux we need to specificate
+	 * order
+	 * 
+	 * @param conference not {@code null}.
+	 * 
+	 * @throws URISyntaxException
+	 */
 
-  public static PropertyList<Property> conferenceToPropertyFruux(Conference conference)
-      throws URISyntaxException {
-    Property urlz, description, location, startDate, endDate, uid, name;
-    PropertyList<Property> propertyList = new PropertyList<>();
-    startDate = new DtStart(new Date(java.util.Date.from(conference.getStartDate())));
-    endDate = new DtEnd(new Date(java.util.Date.from(conference.getEndDate())));
-    uid = new Uid(conference.getUid().toLowerCase());
-    name = new Summary(conference.getTitle());
+	public static PropertyList<Property> conferenceToPropertyFruux(Conference conference) throws URISyntaxException {
+		Property urlz, description, location, startDate, endDate, uid, name;
+		PropertyList<Property> propertyList = new PropertyList<>();
+		startDate = new DtStart(new Date(java.util.Date.from(conference.getStartDate())));
+		endDate = new DtEnd(new Date(java.util.Date.from(conference.getEndDate())));
+		uid = new Uid(conference.getUid().toLowerCase());
+		name = new Summary(conference.getTitle());
 
-    if (conference.getUrl().isPresent()) {
-      urlz = new Url(conference.getUrl().get().toURI());
-      propertyList.add(urlz);
-    }
-    propertyList.add(name);
-    if (conference.getFeeRegistration().isPresent()) {
-      description = new Description("Fee:" + conference.getFeeRegistration().get());
-      propertyList.add(description);
-    }
-    if (!((conference.getCity().isEmpty()) && (conference.getCountry().isEmpty()))) {
-      location = new Location(conference.getCity() + "," + conference.getCountry());
-      propertyList.add(location);
-    }
-    if (!(conference.getParticipants().isEmpty())) {
-      String attendee = conference.getParticipants();
-      Attendee participant =
-          new Attendee(URI.create(attendee.substring(1, (attendee.length() - 1))));
-      propertyList.add(participant);
-    }
-    propertyList.add(startDate);
-    propertyList.add(endDate);
-    propertyList.add(uid);
-    return propertyList;
-  }
+		if (conference.getUrl().isPresent()) {
+			urlz = new Url(conference.getUrl().get().toURI());
+			propertyList.add(urlz);
+		}
+		propertyList.add(name);
+		if (conference.getFeeRegistration().isPresent()) {
+			description = new Description("Fee:" + conference.getFeeRegistration().get());
+			propertyList.add(description);
+		}
+		if (!((conference.getCity().isEmpty()) && (conference.getCountry().isEmpty()))) {
+			location = new Location(conference.getCity() + "," + conference.getCountry());
+			propertyList.add(location);
+		}
+		if (!(conference.getParticipants().isEmpty())) {
 
-  /**
-   * Transform a Conference into a Property List to be used after in VEvent(in the case of
-   * communication with server ) or Event (In the case of IcsFile)
-   * 
-   * @param conference not {@code null}.
-   * 
-   * @throws URISyntaxException
-   */
+			Set<String> it2 = conference.getParticipantsSet();
+			for (String parti : it2) {
+				String attendee = parti;
+				Attendee participant = new Attendee(URI.create(attendee.substring(0, (attendee.length()))));
+				propertyList.add(participant);
+			}
+		}
+		propertyList.add(startDate);
+		propertyList.add(endDate);
+		propertyList.add(uid);
+		return propertyList;
+	}
 
-  public static PropertyList<Property> conferenceToProperty(Conference conference)
-      throws URISyntaxException {
-    Property urlz, description, location, startDate, endDate, uid, sequence, created, dtstamp,
-        lastModified, name;
-    PropertyList<Property> propertyList = new PropertyList<>();
-    startDate = new DtStart(new Date(java.util.Date.from(conference.getStartDate())));
-    endDate = new DtEnd(new Date(java.util.Date.from(conference.getEndDate())));
-    uid = new Uid(conference.getUid().toLowerCase());
-    sequence = new Sequence(2);
-    created = new Created();
-    dtstamp = new DtStamp();
-    lastModified = new LastModified();
-    name = new Summary(conference.getTitle());
+	/**
+	 * Transform a Conference into a Property List to be used after in VEvent(in the
+	 * case of communication with server ) or Event (In the case of IcsFile)
+	 * 
+	 * @param conference not {@code null}.
+	 * 
+	 * @throws URISyntaxException
+	 */
 
-    propertyList.add(created);
-    propertyList.add(dtstamp);
-    propertyList.add(lastModified);
-    propertyList.add(sequence);
-    propertyList.add(uid);
-    propertyList.add(startDate);
-    propertyList.add(endDate);
+	public static PropertyList<Property> conferenceToProperty(Conference conference) throws URISyntaxException {
+		Property urlz, description, location, startDate, endDate, uid, sequence, created, dtstamp, lastModified, name;
+		PropertyList<Property> propertyList = new PropertyList<>();
+		startDate = new DtStart(new Date(java.util.Date.from(conference.getStartDate())));
+		endDate = new DtEnd(new Date(java.util.Date.from(conference.getEndDate())));
+		uid = new Uid(conference.getUid().toLowerCase());
+		sequence = new Sequence(2);
+		created = new Created();
+		dtstamp = new DtStamp();
+		lastModified = new LastModified();
+		name = new Summary(conference.getTitle());
 
-    propertyList.add(name);
-    if (!((conference.getCity().isEmpty()) && (conference.getCountry().isEmpty()))) {
-      location = new Location(conference.getCity() + "," + conference.getCountry());
-      propertyList.add(location);
-    }
-    if (conference.getFeeRegistration().isPresent()) {
-      description = new Description("Fee:" + conference.getFeeRegistration().get());
-      propertyList.add(description);
-    }
-    if (conference.getUrl().isPresent()) {
-      urlz = new Url(conference.getUrl().get().toURI());
-      propertyList.add(urlz);
-    }
+		propertyList.add(created);
+		propertyList.add(dtstamp);
+		propertyList.add(lastModified);
+		propertyList.add(sequence);
+		propertyList.add(uid);
+		propertyList.add(startDate);
+		propertyList.add(endDate);
 
-    return propertyList;
-  }
+		propertyList.add(name);
+		if (!((conference.getCity().isEmpty()) && (conference.getCountry().isEmpty()))) {
+			location = new Location(conference.getCity() + "," + conference.getCountry());
+			propertyList.add(location);
+		}
+		if (conference.getFeeRegistration().isPresent()) {
+			description = new Description("Fee:" + conference.getFeeRegistration().get());
+			propertyList.add(description);
+		}
+		if (conference.getUrl().isPresent()) {
+			urlz = new Url(conference.getUrl().get().toURI());
+			propertyList.add(urlz);
+		}
+		if (!(conference.getParticipants().isEmpty())) {
+			String attendee = conference.getParticipants();
+			Attendee participant = new Attendee(URI.create(attendee.substring(1, (attendee.length() - 1))));
+			propertyList.add(participant);
+		}
 
-  /**
-   * Add the conference in the ics file file (if it exists)
-   * 
-   * @param calFile not {@code null}.
-   * @param conference not {@code null}.
-   * @throws IOException
-   * @throws ParserException
-   * @throws ValidationException
-   * @throws URISyntaxException
-   * @throws ParseException
-   */
+		return propertyList;
+	}
 
-  public static void addConference(String calFile, Conference conference)
-      throws IOException, ParserException, ValidationException, URISyntaxException {
-    Objects.requireNonNull(calFile);
-    Objects.requireNonNull(conference);
+	/**
+	 * Add the conference in the ics file file (if it exists)
+	 * 
+	 * @param calFile    not {@code null}.
+	 * @param conference not {@code null}.
+	 * @throws IOException
+	 * @throws ParserException
+	 * @throws ValidationException
+	 * @throws URISyntaxException
+	 * @throws ParseException
+	 */
 
-    Calendar calendar = new Calendar();
-    calendar = openCalendar(calFile);
+	public static void addConference(String calFile, Conference conference)
+			throws IOException, ParserException, ValidationException, URISyntaxException {
+		Objects.requireNonNull(calFile);
+		Objects.requireNonNull(conference);
 
-    // Creating an event
-    PropertyList<Property> propertyList = new PropertyList<>();
-    propertyList = conferenceToProperty(conference);
-    XComponent meeting = new XComponent("CONFERENCE", propertyList);
-    // add event to the calendar
-    calendar.getComponents().add(meeting);
+		Calendar calendar = new Calendar();
+		calendar = openCalendar(calFile);
 
-    // Saving an iCalendar file
-    saveIcsFile(calendar, calFile);
-  }
+		// Creating an event
+		PropertyList<Property> propertyList = new PropertyList<>();
+		propertyList = conferenceToProperty(conference);
+		XComponent meeting = new XComponent("CONFERENCE", propertyList);
+		// add event to the calendar
+		calendar.getComponents().add(meeting);
 
-  /**
-   * Save the given conference in the ics File
-   * 
-   * @param cal not {@code null}.
-   * @param calFile not {@code null}.
-   * @throws URISyntaxException
-   * @throws ValidationException
-   * @throws IOException
-   */
-  private static void saveIcsFile(Calendar cal, String calFile)
-      throws URISyntaxException, ValidationException, IOException {
-    Objects.requireNonNull(calFile);
-    Objects.requireNonNull(cal);
+		// Saving an iCalendar file
+		saveIcsFile(calendar, calFile);
+	}
 
-    URL resourceUrl = ConferenceWriter.class.getResource(calFile + ".ics");
-    File file = new File(resourceUrl.toURI());
-    try (FileOutputStream fout = new FileOutputStream(file)) {
-      CalendarOutputter outputter = new CalendarOutputter();
-      outputter.setValidating(false);
-      outputter.output(cal, fout);
-    }
-  }
+	/**
+	 * Save the given conference in the ics File
+	 * 
+	 * @param cal     not {@code null}.
+	 * @param calFile not {@code null}.
+	 * @throws URISyntaxException
+	 * @throws ValidationException
+	 * @throws IOException
+	 */
+	private static void saveIcsFile(Calendar cal, String calFile)
+			throws URISyntaxException, ValidationException, IOException {
+		Objects.requireNonNull(calFile);
+		Objects.requireNonNull(cal);
+
+		URL resourceUrl = ConferenceWriter.class.getResource(calFile + ".ics");
+		File file = new File(resourceUrl.toURI());
+		try (FileOutputStream fout = new FileOutputStream(file)) {
+			CalendarOutputter outputter = new CalendarOutputter();
+			outputter.setValidating(false);
+			outputter.output(cal, fout);
+		}
+	}
 }
